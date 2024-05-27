2f6f11bd960c4ffa779f83f627d4106493011b62
Cristtsirc <cristian.sangucho@epn.edu.ec>
2024-05-26 19:36:55 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Usuario usuarioTest = new Usuario("Pepepicapapas");
+        Album album = new Album("album1", "2000", "disquera1", {""});
+        Cancion cancion = new Cancion("pompas de jabon",Duration.ofMinutes(5).plusSeconds(30), "Ricardo Arjona");
+
+        usuarioTest.agregarAlbum("1", "2000", "polirecords", [""]);
+        usuarioTest.agregarCancionaAlbumExistente("album1", cancion)
+    }
+}
+
---
+++ src/Usuario.java
@@ -0,0 +1,7 @@
+public class Usuario {
+    private String nombre;
+
+    public Usuario(String nombre) {
+        this.name = nombre;
+    }
+}
