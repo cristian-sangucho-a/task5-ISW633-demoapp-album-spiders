2485a7202a52b81163a992db0ec4ca003a54e241
Cristian-Zambrano <94475992+Cristian-Zambrano@users.noreply.github.com>
2024-05-29 21:19:26 -0500

    add JavaDoc
    add: he añadido el JavaDoc correspondiente a la documentación del sistema.

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"logic","l":"Album"},{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"logic","l":"Biblioteca"},{"p":"logic","l":"Cancion"},{"p":"<Unnamed>","l":"Main"},{"p":"logic","l":"MenuReproductor"}];updateSearchResults();
